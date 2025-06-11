671877e7c0ea446a39c11b06d3362e8115dbbbc2
Jordi Keijzers <jordikeijzers@gmail.com>
2025-06-11 16:12:51 +0200

    Remove boilerplate code and setup home page

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -1,18 +1,5 @@
-<script setup lang="ts">
-import AppLayout from '@/layouts/app/AppSidebarLayout.vue';
-import type { BreadcrumbItemType } from '@/types';
-
-interface Props {
-    breadcrumbs?: BreadcrumbItemType[];
-}
-
-withDefaults(defineProps<Props>(), {
-    breadcrumbs: () => [],
-});
-</script>
-
 <template>
-    <AppLayout :breadcrumbs="breadcrumbs">
+    <main>
         <slot />
-    </AppLayout>
+    </main>
 </template>
---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+    import AppLayout from '@/layouts/AppLayout.vue';
+</script>
+
+<template>
+    <AppLayout>
+        <h1>Home page</h1>
+    </AppLayout>
+</template>
