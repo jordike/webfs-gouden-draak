8fb1d769357208b6b83348a5f18cf1ffbdd450dc
Jordi Keijzers <jordikeijzers@gmail.com>
2025-06-17 16:08:30 +0200

    Add news

--- resources/js/pages/News.vue
+++ resources/js/pages/News.vue
@@ -4,6 +4,11 @@
 
 <template>
     <AppLayout>
-        <h1>News</h1>
+        <section class="p- mx-[50px] border-1 border-black bg-white p-[5px] text-center font-bold text-black">
+            <p class="my-[20px]">
+                Door de Corona crisis is De Gouden Draak op het moment slechts beperkt open. <br />
+                Het restaurant-gedeelte is gesloten. U kan uw favoriete gerechten nog wel afhalen.
+            </p>
+        </section>
     </AppLayout>
 </template>
