066d7f3464d15144d1342944dc57a18014a78d97
Jordi Keijzers <jordikeijzers@gmail.com>
2025-06-18 00:10:39 +0200

    Add sales overview

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -2,12 +2,19 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
 use Illuminate\Http\Request;
 
 class SalesController extends Controller
 {
     public function index()
     {
-        return inertia('BackOffice/Sales/Index');
+        $orders = Order::with('items.menuItem')
+            ->orderBy('date_placed', 'desc')
+            ->get();
+
+        return inertia('BackOffice/Sales/Index', [
+            'orders' => $orders
+        ]);
     }
 }
