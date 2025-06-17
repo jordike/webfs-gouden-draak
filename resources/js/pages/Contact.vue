0937daf3b109f0728be5e7ee6fc2563ee8b02ccf
Jordi Keijzers <jordikeijzers@gmail.com>
2025-06-17 16:31:56 +0200

    Add contact page

--- resources/js/pages/Contact.vue
+++ resources/js/pages/Contact.vue
@@ -4,6 +4,16 @@
 
 <template>
     <AppLayout>
-        <h1>Contact</h1>
+        <div class="flex flex-col">
+            <section class="border-1 border-black bg-white p-[5px] text-center font-bold text-black">
+                <p class="my-[20px]">De Gouden Draak is eenvoudig te vinden, vlak bij het centrum, 5 minuten lopen achter het centraal station.</p>
+                <p class="mb-[20px]">
+                    Onderwijsboulevard 215, kamer OG112 <br />
+                    5223 DE 's-Hertogenbosch
+                </p>
+            </section>
+
+            <img class="w-[100%] self-center" src="/assets/img/map.png" alt="Locatie van de Gouden Draak op de kaart" />
+        </div>
     </AppLayout>
 </template>
