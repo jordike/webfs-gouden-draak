2c45d009506fd4f1cab78961df66fd44ee09b59d
Jordi Keijzers <jordikeijzers@gmail.com>
2025-06-19 11:02:30 +0200

    Bug fixes

--- resources/js/components/BackOffice/Orders/MenuItemCard.vue
+++ resources/js/components/BackOffice/Orders/MenuItemCard.vue
@@ -10,14 +10,18 @@
 
 <template>
     <div class="flex items-center justify-between rounded-xl border border-gray-100 bg-white p-5 shadow transition hover:shadow-lg">
-        <div class="mb-1">
-            <span class="inline-block rounded bg-green-100 px-2 py-0.5 font-mono text-xs font-bold text-green-700 shadow-sm"> #{{ item.id }} </span>
+        <div class="flex flex-col">
+            <div class="mb-1">
+                <span class="inline-block rounded bg-green-100 px-2 py-0.5 font-mono text-xs font-bold text-green-700 shadow-sm">
+                    #{{ item.id }}
+                </span>
+            </div>
+            <div class="flex items-center gap-2 font-semibold text-gray-800">
+                <span v-html="item.name"></span>
+            </div>
+            <div v-if="item.description" class="mt-1 text-sm text-gray-500" v-html="item.description"></div>
+            <div class="mt-1 font-bold text-green-600">€{{ item.price }}</div>
         </div>
-        <div class="flex items-center gap-2 font-semibold text-gray-800">
-            <span v-html="item.name"></span>
-        </div>
-        <div v-if="item.description" class="mt-1 text-sm text-gray-500" v-html="item.description"></div>
-        <div class="mt-1 font-bold text-green-600">€{{ item.price }}</div>
 
         <button
             class="ml-3 rounded-full bg-gradient-to-br from-green-500 to-green-600 p-3 text-white shadow transition hover:from-green-600 hover:to-green-700 focus:ring-2 focus:ring-green-300 focus:outline-none"
